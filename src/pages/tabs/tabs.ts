c27a4b14bd3f3f46fb001648805a36da9929cec7
varsha <varshab13bavane@gmail.com>
2017-12-09 09:34:46 +0530

    bug fxd in tab page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import {}
 import { CaameraPage } from '../caamera/caamera';
 import { StausPage } from '../staus/staus';
 import { CallsPage } from '../calls/calls';
